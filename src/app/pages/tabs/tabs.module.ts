a1be296a2543918c7d1ed7a61ad9a01e776d1082
peterennis <peterennis@yahoo.com>
2018-07-11 23:22:43 -0700

    feature() update to 4.0.0-alpha.9

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -1,6 +1,8 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TabsPageRoutingModule } from './tabs.router.module';
 
@@ -12,6 +14,8 @@ import { HomePageModule } from '../home/home.module';
 @NgModule({
   imports: [
     IonicModule,
+    CommonModule,
+    FormsModule,
     TabsPageRoutingModule,
     HomePageModule,
     AboutPageModule,
