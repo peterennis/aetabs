ac4c9f6e05b002092bbc6b12f8b208b100f20b26
peterennis <peterennis@yahoo.com>
2019-03-05 21:55:44 -0800

    fix () could not find Routes

--- src/app/aeplaces/aeoffers/aenew-offer/aenew-offer.module.ts
+++ src/app/aeplaces/aeoffers/aenew-offer/aenew-offer.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -17,7 +16,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
--- src/app/aeplaces/aeplaces-routing.module.ts
+++ src/app/aeplaces/aeplaces-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AeplacesPage } from './aeplaces.page';
 
 const routes: Routes = [
