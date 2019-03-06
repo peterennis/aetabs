ee604acfac1b0073547837e6c3f4324f3ad4aaa5
peterennis <peterennis@yahoo.com>
2019-03-05 21:46:26 -0800

    fix () aeplaces routing

--- src/app/aeplaces/aeplaces.module.ts
+++ src/app/aeplaces/aeplaces.module.ts
@@ -1,26 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { AeplacesPage } from './aeplaces.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: AeplacesPage
-  }
-];
+import { AeplacesRoutingModule } from './aeplaces-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    AeplacesRoutingModule
   ],
   declarations: [AeplacesPage]
 })
-export class AeplacesPageModule {}
+export class AeplacesPageModule { }
