cb6b0f7f1e5be86128000531388894e78ef732dd
peterennis <peterennis@yahoo.com>
2019-03-05 21:38:01 -0800

    feat (routes) add routes for places

---
+++ src/app/aeplaces/aeplaces-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AeplacesPage } from './aeplaces.page';
+
+const routes: Routes = [
+    {
+        path: 'tabs',
+        component: AeplacesPage,
+        children: [{
+            path: 'discover', children: [
+                {
+                    path: '',
+                    loadChildren: './aediscover/aediscover.module#AediscoverPageModule'
+                },
+                {
+                    path: ':placeId',
+                    loadChildren: './aediscover/aeplace-detail/aeplace-detail.module#AeplaceDetailPageModule'
+                }
+            ]
+        }]
+    },
+    {
+        path: 'offers',
+        children: [
+            {
+                path: '',
+                loadChildren: './aeoffers/aeoffers.module#AeoffersPageModule'
+            },
+            {
+                path: 'new',
+                loadChildren: './aeoffers/aenew-offer.module#AenewOfferPageModule'
+            },
+            {
+                path: 'edit/:placeId',
+                loadChildren: './aeoffers/aeedit-offer.module#AeeditOfferPageModule'
+            },
+            {
+                path: 'placeId',
+                loadChildren: './aeoffers/aeoffer-bookings/aeoffer-bookings.module#AeofferBookingsPageModule'
+            }
+        ]
+    }
+]
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AeplacesRoutingModule { }
