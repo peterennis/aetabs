ebad4b0a40586550f6a892014be5521811dab165
peterennis <peterennis@yahoo.com>
2019-03-06 19:48:19 -0800

    fix (forms) remove unused forms module

--- src/app/aeauth/aeauth.module.ts
+++ src/app/aeauth/aeauth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
@@ -17,10 +16,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
   declarations: [AeauthPage]
 })
-export class AeauthPageModule {}
+export class AeauthPageModule { }
