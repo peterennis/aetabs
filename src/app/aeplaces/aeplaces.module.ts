130ad07ca8125db6e4e5e2c5f8022db727f811ee
peterennis <peterennis@yahoo.com>
2019-03-07 16:00:51 -0800

    fix () import Routes and RouterModule

--- src/app/aeplaces/aeplaces.module.ts
+++ src/app/aeplaces/aeplaces.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
