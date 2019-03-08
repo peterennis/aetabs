66a7603d4c519dc65a0dd19e07b9d1af0b787b35
peterennis <peterennis@yahoo.com>
2019-03-07 19:36:32 -0800

    WIP (route) temp workaround for relative build error offers new and edit

--- src/app/aeplaces/aeplaces-routing.module.ts
+++ src/app/aeplaces/aeplaces-routing.module.ts
@@ -3,47 +3,61 @@ import { Routes, RouterModule } from '@angular/router';
 import { AeplacesPage } from './aeplaces.page';
 
 const routes: Routes = [
-    {
-        path: 'tabs',
-        component: AeplacesPage,
-        children: [{
-            path: 'discover', children: [
-                {
-                    path: '',
-                    loadChildren: './aediscover/aediscover.module#AediscoverPageModule'
-                },
-                {
-                    path: ':placeId',
-                    loadChildren: './aediscover/aeplace-detail/aeplace-detail.module#AeplaceDetailPageModule'
-                }
-            ]
-        }]
-    },
-    {
+  {
+    path: './aeplaces/tabs',
+    component: AeplacesPage,
+    children: [
+      {
+        path: 'discover', children: [
+          {
+            path: '',
+            loadChildren: './aediscover/aediscover.module#AediscoverPageModule'
+          },
+          {
+            path: ':placeId',
+            loadChildren: './aediscover/aeplace-detail/aeplace-detail.module#AeplaceDetailPageModule'
+          }
+        ]
+      },
+      {
         path: 'offers',
         children: [
-            {
-                path: '',
-                loadChildren: './aeoffers/aeoffers.module#AeoffersPageModule'
-            },
-            {
-                path: 'new',
-                loadChildren: './aeoffers/aenew-offer.module#AenewOfferPageModule'
-            },
-            {
-                path: 'edit/:placeId',
-                loadChildren: './aeoffers/aeedit-offer.module#AeeditOfferPageModule'
-            },
-            {
-                path: 'placeId',
-                loadChildren: './aeoffers/aeoffer-bookings/aeoffer-bookings.module#AeofferBookingsPageModule'
-            }
+          {
+            path: '',
+            loadChildren: './aeoffers/aeoffers.module#AeoffersPageModule'
+          },
+          /*
+          {
+            path: 'new',
+            loadChildren: './aeoffers/aenew-offer.module#AenewOfferPageModule'
+          },
+          {
+            path: 'edit/:placeId',
+            loadChildren: './aeoffers/aeedit-offer.module#AeeditOfferPageModule'
+          },
+          */
+          {
+            path: 'placeId',
+            loadChildren: './aeoffers/aeoffer-bookings/aeoffer-bookings.module#AeofferBookingsPageModule'
+          }
         ]
-    }
-]
+      },
+      {
+        path: '',
+        redirectTo: '/aeplaces/tabs/discover',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: './aeplaces/tabs/discover',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class AeplacesRoutingModule { }
