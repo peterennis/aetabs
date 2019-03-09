35c26d88c07bc412c6af6c8b6a244a885597cffc
peterennis <peterennis@yahoo.com>
2019-03-08 21:17:26 -0800

    fix () use :placeId

--- src/app/aeplaces/aeplaces-routing.module.ts
+++ src/app/aeplaces/aeplaces-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
           },
           */
           {
-            path: 'placeId',
+            path: ':placeId',
             loadChildren: './aeoffers/aeoffer-bookings/aeoffer-bookings.module#AeofferBookingsPageModule'
           }
         ]
