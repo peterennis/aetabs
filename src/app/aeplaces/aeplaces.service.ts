0fb0686b59c45d66c8e324e18d02237e78ed3a4f
peterennis <peterennis@yahoo.com>
2019-03-09 16:51:12 -0800

    feat () add some places data

--- src/app/aeplaces/aeplaces.service.ts
+++ src/app/aeplaces/aeplaces.service.ts
@@ -6,7 +6,29 @@ import { Aeplaces } from './aeplaces.model';
   providedIn: 'root'
 })
 export class AeplacesService {
-  private _places: Aeplaces[] = [];
+  private _places: Aeplaces[] = [
+    new Aeplaces(
+      'P1',
+      'Universal Studios, Hollywood',
+      'California - The Golden State',
+      'https://www.universalstudioshollywood.com/',
+      400.40
+    ),
+    new Aeplaces(
+      'P2',
+      'The Statue of Liberty',
+      'Liberty Island - New York Harbor',
+      'https://www.nps.gov/stli/index.htm',
+      1200.12
+    ),
+    new Aeplaces(
+      'P2',
+      'Washington Monument',
+      'National Mall in Washington, D.C',
+      'https://www.nationalparks.org/explore-parks/washington-monument',
+      888.88
+    )
+  ];
 
   get places() {
     return [...this._places];
