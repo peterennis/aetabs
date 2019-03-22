1a724c8862cfb739a8655419c95ff046b3700d3c
peterennis <peterennis@yahoo.com>
2019-03-21 21:03:50 -0700

    fix (service) use in constructor

--- src/app/aeplaces/aediscover/aediscover.page.ts
+++ src/app/aeplaces/aediscover/aediscover.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Aeplaces } from '../aeplaces.model';
+import { AeplacesService } from '../aeplaces.service';
+
 @Component({
   selector: 'app-aediscover',
   templateUrl: './aediscover.page.html',
   styleUrls: ['./aediscover.page.scss'],
 })
 export class AediscoverPage implements OnInit {
+  loadedPlaces: Aeplaces[];
 
-  constructor() { }
+  constructor(private myPlacesService: AeplacesService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.myPlacesService.places;
   }
 
 }
